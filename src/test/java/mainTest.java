09083caf09119af4911c6f8d7bb3fb61ff3eb204
joly_h <joly_h@bitbucket.org>
2018-03-02 16:37:02 +0100

    2 eal uocuoc

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -5,7 +5,16 @@ import junit.framework.*;
 
 
 public class mainTest extends TestCase {
-    public void testmain() throws Exception {
-        fail("lol");
+    LinkedList<String> test = new LinkedList<String>("a");
+    public void testCreation() throws Exception {
+        assertNotNull(test);
+    }
+    public void testAddTail() throws Exception {
+        test.addTail(new LinkedList<String>("b"));
+        assertEquals("b", test.getTail().getData());
+    }
+    public void testHead() throws Exception {
+        test.addHead(new LinkedList<String>("b"));
+        assertEquals("b", test.getHead().getData());
     }
 }
